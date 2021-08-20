0f8e42fd300c789d9825bae78d1b012be1fc3911
a-givertzman <a.givertzman@icloud.com>
2021-08-20 17:50:28 +0300

    landing page and calc added

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
-import { 
-    signInWithEmailAndPassword, 
-    signUpWithEmailAndPassword, 
-    getAuthForm, 
-    renderModalAfterAuth 
-} from './auth';
-import { Booking, bookingStatus, renderBookings } from './booking';
-import { closeModalWindow, createModalWindow, hideElement, renderAuthBtn, renderSelectUserBtn, showElement } from './ui-util';
-import { 
-    validateAddressInput, 
-    validateDateInput, 
-    validateNumberInput, 
-    validateWorkDescriptionInput 
-} from './value_validators';
 import { clearCookie, getCookie, setCookie } from './cookie';
-import './styles.css';
+import '@plugins/slick-slider/slick.css';
+import '@plugins/jquery-form-styler/jquery.formstyler.css';
+import '@plugins/jquery-form-styler/jquery.formstyler.theme.css';
+import './css/style.css';
+import './css/media.css';
+// import slider_1_background_img from '@img/slider-background.png';
+// import header_logo_img from '@img/header-logo.png';
 import { User } from './user';
 
+$(function() {
+    // MAIN SLIDER
+    $('.slider-items').slick({
+        prevArrow: '<button type="button" class="slick-prev slider-nav-btn"></button>',
+        nextArrow: '<button type="button" class="slick-next slider-nav-btn"></button>',
+        autoplay: false,
+        infinite: false,
+    });
+    // NEWS SLIDER
+    $('.news-slider-items').slick({
+        // dots: true,
+        prevArrow: '<button type="button" class="news-slick-prev slider-nav-btn"></button>',
+        nextArrow: '<button type="button" class="news-slick-next slider-nav-btn"></button>',
+        autoplay: false,
+        infinite: false,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        slidesToShow: 3,
+        initialSlide: 1,
+    });
+});
+
+
+// JQUERY FORM STYLER
+(function($) {
+    $(function() {
+        $('select').styler({
+            selectSmartPositioning: true,
+        });
+    });
+})(jQuery);
 
 const form = document.querySelector('#booking-form');
 const dateInput = document.querySelector('#date-input');
@@ -26,6 +50,8 @@ const workDescriptionInput = document.querySelector('#work-description-input');
 const submitBtn = document.querySelector('#submit-btn');
 const authBtn = document.querySelector('#auth-btn');
 const showAllBtn = document.querySelector('#show-all-btn');
+const showMenuBtn = document.querySelector('#header-menu-btn');
+
 
 var USER = {};
 var ID_TOKEN;
@@ -40,10 +66,22 @@ Date.prototype.toDateInputValue = (function() {
 
 //
 var isValid = false;
-submitBtn.disabled = true;
+// submitBtn.disabled = true;
 let date = new Date();
-dateInput.value = date;
-timeInput.value = `${date.getHours()}:${date.getMinutes()}`;
+// dateInput.value = date;
+// timeInput.value = `${date.getHours()}:${date.getMinutes()}`;
+
+
+// click on menu icon
+showMenuBtn.addEventListener('click',(event) => {
+    let menu = document.querySelector('.menu ul');
+    let menuStyle = window.getComputedStyle(menu);
+    if (menuStyle.display == 'none') {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none';
+    }
+});
 
 window.addEventListener(                                            // ON LOAD WINDOW
     'load', (event) => {
@@ -53,234 +91,12 @@ window.addEventListener(                                            // ON LOAD W
         // console.log('[window.onLoad] userName:', userName);
         ID_TOKEN = USER.email ? getCookie(USER.email) : null;
         // console.log('[window.onLoad] idToken:', idToken);
-        
-        setFormState(USER);
-});
-
-form.addEventListener('submit', submitFormHandler);
-authBtn.addEventListener('click', signInHandler);
-showAllBtn.addEventListener('click', showAllHandler);
-
-//
-function signInHandler(event) {
-    signInWindow = createModalWindow('Авторизация', getAuthForm(), 'modal-window');
-
-    let modalForm = document.querySelector('#auth-form');
-    modalForm.addEventListener('submit', submitAuthFormHandler);
-
-    let modalSignUpForm = document.querySelector('#sign-up-form');
-    modalSignUpForm.addEventListener('submit', submitSignUpFormHandler);
-}
-
-//
-function submitSignUpFormHandler(event) {
-    console.log('submiiting sign up form');
-    event.preventDefault();
-    const name = event.target.querySelector('#name-input').value;
-    const email = event.target.querySelector('#email-input').value;
-    const password = event.target.querySelector('#password-input').value;
-    const signUpFormBtn = event.target.querySelector('#sign-up-form-btn');
-    signUpFormBtn.disabled = true;
-    console.log('[submitSignUpFormHandler] name', name);
-    console.log('[submitSignUpFormHandler] email', email);
-    console.log('[submitSignUpFormHandler] password', password);
-    signUpWithEmailAndPassword(name, email, password)
-    .then((response) => {
-        if (response.error) {                                       // error message
-            createModalWindow(
-                'Ошибка', 
-                renderModalAfterAuth(response.error.message),
-                'modal-window'
-            );
-            clearCookie();
-        } else {                                                    // authorized
-            closeModalWindow();
-            setCurrentUser(response.user, response.idToken);
-            renderBookings(ID_TOKEN, USER.email);
-        }
-        signUpFormBtn.disabled = false;
-    })
-}
-
-//
-function submitAuthFormHandler(event) {
-    console.log('submiiting auth form');
-    event.preventDefault();
-    const email = event.target.querySelector('#email-input').value;
-    const password = event.target.querySelector('#password-input').value;
-    const signInFormBtn = event.target.querySelector('#sign-in-form-btn');
-    signInFormBtn.disabled = true;
-    console.log('[submitAuthFormHandler] email', email);
-    console.log('[submitAuthFormHandler] password', password);
-    signInWithEmailAndPassword(email, password)
-    .then((response) => {
-        if (response.error) {                                       // error message
-            createModalWindow(
-                'Ошибка авторизации', 
-                renderModalAfterAuth(response.error.message),
-                'modal-window',
-            );
-            clearCookie();
-        } else {                                                    // authorized
-            User.getUser(email, response.idToken)
-                .then(user => {
-                    closeModalWindow();
-                    setCurrentUser(user, response.idToken);
-                    renderBookings(ID_TOKEN, USER.email);
-                });
-        }
-        signInFormBtn.disabled = false;
-    })
-}
-
-//
-export function submitSelectUserHandler(event) {
-    console.log('[submitSelectUserHandler] target: ', event.target.parentElement);
-    let email = event.target.parentElement.getAttribute('email');
-    console.log('[submitSelectUserHandler] email: ', email);
-    renderBookings(ID_TOKEN, email);
-}
-
-//
-function setCurrentUser(user, token) {
-    console.log('[setCurrentUser] user', user);
-    ID_TOKEN = token;
-    USER = user;
-    setCookie('name', USER.name, 1);
-    setCookie('group', USER.group, 1);
-    setCookie('email', USER.email, 1);
-    setCookie(USER.email, ID_TOKEN, 1);
-    setFormState(USER);
-}
-
-//
-function setFormState(user) {
-    
-    let userIsAdmin = (user.group && user.group == 'admin') ? true : false;
-    
-    // authBtn.innerHTML = renderAuthBtn(user.name, user.email);
-    authBtn.innerHTML = USER.email ? renderAuthBtn(USER.name, USER.email) : 'Авторизация';
-    showAllBtn.disabled = !userIsAdmin;
-    if (ID_TOKEN) {
-        renderBookings(ID_TOKEN, userIsAdmin ? '' : USER.email);
-        if (userIsAdmin) {
-            showElement(document.querySelector('#user-navigator-top'));
-            showElement(document.querySelector('#user-navigator'));
-            User.getUsers(ID_TOKEN)
-            .then(users => renderSelectUserBtn(users));
-        } else {
-            hideElement(document.querySelector('#user-navigator-top'));
-            hideElement(document.querySelector('#user-navigator'));
-        }
-    } else {
-        signInHandler();
-    }
-}
-
-//
-function showAllHandler(event) {
-
-}
-
-function submitFormHandler(event) {
-    event.preventDefault();
-    
-    let booking = {
-        date: '',
-        time: '',
-        dauration: '',
-        address: '',
-        workDescription: '',
-        status: bookingStatus.pending,
-        comments: [],
-    }
-
-    let date = new Date(dateInput.value + ` ${timeInput.value}:00`);
-    console.log('[submitFormHandler] date: ', date);
-    
-    booking = {
-        date: date.toJSON(),
-        dauration: daurationInput.value,
-        address: addressInput.value.trim(),
-        workDescription: workDescriptionInput.value.trim(),
-        status: bookingStatus.pending,
-        user: USER.email,
-    }
-
-    console.log('submiting....');
-    
-    submitBtn.disabled = true;
-    
-    Booking.create(booking, ID_TOKEN, USER.email)
-        .then(response => {
-            console.log('[submitFormHandler] response:', response);
-            console.log('[submitFormHandler] booking:', booking);
-            
-            submitBtn.disabled = false;
-            dateInput.value = new Date().toDateInputValue();
-            timeInput.value = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-            daurationInput.value = 1;
-            addressInput.value = '';
-            workDescriptionInput.value = '';
-            renderBookings(ID_TOKEN, USER.email);
-        });
-}
-
-function validateAllFields() {
-    let isValid = true;
-
-    if (validateDateInput(dateInput.value, true)) {
-        console.log(`[validateAllFields] date: ${dateInput.value} - valid`);
-    } else {
-        isValid = false;
-    }
-    
-    if (validateNumberInput(daurationInput.value, true)) {
-        console.log(`[validateAllFields] dauration: ${daurationInput.value} - valid`);
-    } else {
-        isValid = false;
-    }
-
-    if (validateAddressInput(addressInput.value, true)) {
-        console.log(`[validateAllFields] address: ${addressInput.value} - valid`);
-    } else {
-        isValid = false;
-    }
-
-    if (validateWorkDescriptionInput(workDescriptionInput.value, true)) {
-        console.log(`[validateAllFields] workDescription: ${workDescriptionInput.value} - valid`);
-    } else {
-        isValid = false;
-    }
-
-    if (isValid) {
-        console.log('all valid');
-        submitBtn.disabled = false;
-    } else {
-        submitBtn.disabled = true;
-    }
-}
-
-daurationInput.addEventListener('beforeinput', (event) => {
-    if (event) {
-        if (!validateNumberInput(event.data)) {
-            event.preventDefault();
-        }
-    }
-});
-
-dateInput.addEventListener('input', (event) => {
-    validateAllFields();
-});
-
-daurationInput.addEventListener('input', (event) => {
-    validateAllFields();
-});
-
-addressInput.addEventListener('input', (event) => {
-    validateAllFields();
-});
-
-workDescriptionInput.addEventListener('input', (event) => {
-    validateAllFields();
+        // console.log('[window.onLoad] slider-1: ', document.querySelector('.slider-1'));
+        // console.log('[window.onLoad] slider-1 image: ', header_logo_img);
+        // let img = document.createElement('img');
+        // img.src = slider_1_background_img;
+        // document.querySelector('.slider-1').appendChild(img);
+        // document.querySelector('.header__logo_image').src = header_logo_img;
+        // document.querySelector('.slider-1').style.backgroundImage = slider_1_background_img;
+        // setFormState(USER);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/app.js',
+    entry: {
+        app: [
+            './src/plugins/slick-slider/slick.js',
+            './src/plugins/jquery-form-styler/jquery.formstyler.js',
+            './src/app.js', 
+        ],
+    },
+    optimization: {
+        minimize: false,
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    performance: {
+        // hints: false,
+        maxEntrypointSize: 2512000,
+        maxAssetSize: 2512000,
+    },
     output: {
         filename: 'bundle.[chunkhash].js',
         path: path.resolve(__dirname, 'public')
+    },
+    resolve: {
+        alias: {
+            '@plugins': path.resolve(__dirname, 'src/plugins'),
+            '@fonts': path.resolve(__dirname, 'src/css/fonts'),
+            '@img': path.resolve(__dirname, 'src/img'),
+            '@news': path.resolve(__dirname, 'src/news'),
+            // '@':    path.resolve(__dirname, 'src'),
+        },
     },
     devServer: {
         port: 3000,
@@ -14,15 +41,54 @@ module.exports = {
     },
     plugins: [
         new HTMLPlugin({
-            template: './src/index.html'
+            title: 'Уборка жилых помещений',
+            template: './src/index.html',
+            filename: 'index.html',
         }),
-        new CleanWebpackPlugin()
+        new HTMLPlugin({
+            title: 'Расчет стоимости уборки помещения',
+            template: './src/cleaning-calc.html',
+            filename: 'cleaning-calc.html',
+        }),
+        new HTMLPlugin({
+            title: 'Заказать уборку помещения',
+            template: './src/bookong.html',
+            filename: 'bookong.html',
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
+                // include: path.resolve(__dirname, 'src/css'),
                 use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                include: path.resolve(__dirname, 'src/img/'),
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[hash][ext]'
+                },
+            },
+            // {
+            //     test: /\.sass$/i,
+            //     use: ["sass-loader"],
+            // },
+            {
+                // test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+                include: path.resolve(__dirname, 'src/css/fonts'),
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[hash][ext]'
+                },
+                // use: ['file-loader'],
             },
         ],
     },
